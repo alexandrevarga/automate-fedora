a100d9163211372f2056908aab7d0dc4a82232cf
Alexandre Varga <52091024+alexandrevarga@users.noreply.github.com>
2025-03-19 19:59:40 -0300

    Update install.sh
    Updated to my user case

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Instalar o driver da NVIDIA e CUDA
-sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs -y
-sudo dnf install nvidia-vaapi-driver -y
+#sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs -y
+#sudo dnf install nvidia-vaapi-driver -y
 
 # Corrigir os problemas de codec
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-sudo dnf groupupdate sound-and-video -y
+#sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+#sudo dnf groupupdate sound-and-video -y
 sudo dnf install amrnb amrwb faad2 flac gpac-libs lame libde265 libfc14audiodecoder mencoder x264 x265 ffmpegthumbnailer -y
 
 # Instalar o 1Password
@@ -19,49 +19,48 @@ sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https:/
 sudo dnf install 1password -y
 
 # Instalar o Piper para gerenciar o MX Master 3
-sudo dnf install piper -y
+#sudo dnf install piper -y
 
 # Instalar o GNOME Tweaks para configurar o botão de minimizar
-sudo dnf install gnome-tweaks -y
+#sudo dnf install gnome-tweaks -y
 
 # Instalar o Google Chrome (e remover o aviso de gerenciado pela organização)
-sudo dnf install fedora-workstation-repositories -y
-sudo dnf config-manager --set-enabled google-chrome
-sudo dnf install google-chrome-stable -y
-sudo dnf remove fedora-chromium-config -y
+#sudo dnf install fedora-workstation-repositories -y
+#sudo dnf config-manager --set-enabled google-chrome
+#sudo dnf install google-chrome-stable -y
+#sudo dnf remove fedora-chromium-config -y
 
 # Instalar as fontes da Microsoft
-sudo dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
+#sudo dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
 
 # Instalar ferramentas para jogos
-sudo dnf install steam -y
-flatpak install flathub com.vysp3r.ProtonPlus
-flatpak install flathub com.steamgriddb.steam-rom-manager
-flatpak install flathub com.steamgriddb.SGDBoop
-flatpak install flathub info.cemu.Cemu
-flatpak install flathub com.usebottles.bottles
-flatpak install flathub com.heroicgameslauncher.hgl
-flatpak install flathub dev.lizardbyte.app.Sunshine
+#sudo dnf install steam -y
+#flatpak install flathub com.vysp3r.ProtonPlus
+#flatpak install flathub com.steamgriddb.steam-rom-manager
+#flatpak install flathub com.steamgriddb.SGDBoop
+#flatpak install flathub info.cemu.Cemu
+#flatpak install flathub com.usebottles.bottles
+#flatpak install flathub com.heroicgameslauncher.hgl
+#flatpak install flathub dev.lizardbyte.app.Sunshine
 
 # Instalar aplicativos em flatpak
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub com.spotify.Client
-flatpak install flathub tech.feliciano.pocket-casts
-flatpak install flathub com.obsproject.Studio
-flatpak install flathub io.github.celluloid_player.Celluloid
+#flatpak install flathub com.discordapp.Discord
+#flatpak install flathub com.spotify.Client
+#flatpak install flathub tech.feliciano.pocket-casts
+#flatpak install flathub com.obsproject.Studio
+#flatpak install flathub io.github.celluloid_player.Celluloid
 flatpak install flathub org.gnome.Boxes
 flatpak install flathub com.mattjakeman.ExtensionManager
-flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub org.nickvision.tubeconverter
-flatpak install flathub org.localsend.localsend_app
-flatpak install flathub page.codeberg.libre_menu_editor.LibreMenuEditor
-flatpak install flathub de.haeckerfelix.Fragments
 flatpak install flathub com.rtosta.zapzap
 flatpak install flathub com.todoist.Todoist
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub io.github.pwr_solaar.solaar
-flatpak install flathub io.github.brunofin.Cohesion
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub de.haeckerfelix.Fragments
+flatpak install flathub org.localsend.localsend_app
+flatpak install flathub com.github.tchx84.Flatseal
+#flatpak install flathub org.nickvision.tubeconverter
+flatpak install flathub page.codeberg.libre_menu_editor.LibreMenuEditor
+#flatpak install flathub io.github.pwr_solaar.solaar
+#flatpak install flathub io.github.brunofin.Cohesion
 
 # Instalar as fontes que estão na pasta: Fontes
 # Aplicativos para instalar depois manualmente: DaVinci Resolve, Figma for Desktop, Insync
